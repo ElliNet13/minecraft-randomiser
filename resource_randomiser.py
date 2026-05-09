f034d470a09e4a44239570d099ea1602bc5ca51c
ElliNet13 (PC) <elliotmoose4@gmail.com>
2026-05-09 11:08:14 -0600

    No models get random by default and multi-thread a lot

--- resource_randomiser.py
+++ resource_randomiser.py
@@ -5,6 +5,9 @@ import sys
 import json
 import argparse
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import signal
 
 parser = argparse.ArgumentParser(description='Randomise the assets of a Minecraft resource pack.')
 parser.add_argument('-p', '--pack', default='pack', type=str, dest='pack', help='specifies a resource pack folder')
@@ -14,11 +17,11 @@ parser.add_argument('--alttextures', action='store_true', dest='alttextures', he
 parser.add_argument('--notextures', action='store_false', dest='textures', help='disables randomised textures')
 parser.add_argument('--noblockstates', action='store_false', dest='blockstates', help='disables randomised block states')
 parser.add_argument('--nosounds', action='store_false', dest='sounds', help='disables randomised sounds')
-parser.add_argument('--notexts', action='store_false', dest='texts', help='disables randomised text')
+parser.add_argument('--langs', default='en_us', type=str, dest='langs', help="comma-separated list of language codes to randomize, or 'all', or 'none'. Default: en_us")
 parser.add_argument('--nofonts', action='store_false', dest='fonts', help='disables randomised fonts')
 parser.add_argument('--noshaders', action='store_false', dest='shaders', help='disables randomised shaders')
 parser.add_argument('--install', action='store_true', dest='install', help='auto-install the shuffled pack to the Minecraft resourcepacks folder')
-parser.add_argument('--nomodels', action='store_false', dest='models', help='EXPERIMENTAL: randomised block/item models')
+parser.add_argument('--models', action='store_true', dest='models', help='EXPERIMENTAL: randomised block/item models')
 
 args = parser.parse_args()
 
@@ -29,13 +32,37 @@ randomisemodels = args.models
 #randomiseanimations = args.animations
 randomiseblockstates = args.blockstates
 randomisesounds = args.sounds
-randomisetext = args.texts
+lang_option = args.langs.strip().lower()
 randomisefont = args.fonts
 randomiseshaders = args.shaders
 alttextures = args.alttextures
 auto_install = args.install
 
+cancel_event = threading.Event()
+
+def check_cancel():
+    if cancel_event.is_set():
+        raise KeyboardInterrupt
+
+if lang_option == 'none':
+    randomisetext = False
+    selected_language_codes = []
+elif lang_option == 'all':
+    randomisetext = True
+    selected_language_codes = None
+else:
+    randomisetext = True
+    selected_language_codes = [lang.strip().lower() for lang in lang_option.split(',') if lang.strip()]
+
+signal.signal(signal.SIGINT, lambda sig, frame: cancel_event.set())
+
 random.seed(randomseed)
+
+def signal_handler(sig, frame):
+    print('\nInterrupt received, cleaning up...')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 if resourcepack == "shuffle":
     print("The input resource pack may not be named 'shuffle'.")
@@ -49,9 +76,9 @@ if os.path.exists(os.path.join('shuffle')):
 def makepath(path):
     if not os.path.exists(os.path.dirname(path)):
         try:
-            os.makedirs(os.path.dirname(path))
-        except:
-            print('DEBUG: tried to create an existing folder')
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+        except Exception as e:
+            pass
 
 if not (randomisetextures or randomisemodels or randomiseblockstates or randomisesounds or randomisetext or randomisefont or randomiseshaders):
     print('Successfully randomised nothing!')
@@ -128,6 +155,17 @@ for dirpath, dirs, files in os.walk(os.path.join(resourcepack,"assets")):
 
 print(f"Random Seed: {randomseed}")
 
+if randomisetext:
+    if selected_language_codes is None:
+        langs_to_process = languages
+    else:
+        selected_set = set(selected_language_codes)
+        langs_to_process = [lang for lang in languages if os.path.splitext(os.path.basename(lang))[0].lower() in selected_set]
+        if selected_language_codes and not langs_to_process:
+            print(f"Warning: no matching language files found for {', '.join(selected_language_codes)}")
+else:
+    langs_to_process = []
+
 tasks = []
 randoimagery = randomisetextures or randomisefont
 for resolution, imagefiles in images.items():
@@ -145,7 +183,7 @@ for key, value in shaders.items():
     if randomiseshaders and value:
         tasks.append((value, f"shaders ({key})"))
 
-text_task = randomisetext and (languages or specialtexts)
+text_task = randomisetext and (langs_to_process or specialtexts)
 total_steps = len(tasks) + (1 if text_task else 0)
 
 with tqdm(total=total_steps, desc="Overall Progress") as overall_pbar:
@@ -153,12 +191,20 @@ with tqdm(total=total_steps, desc="Overall Progress") as overall_pbar:
         with tqdm(total=len(toRando), desc=inputType) as pbar:
             shufflelist = list(toRando)
             random.shuffle(shufflelist)
-            for i, orig_file in enumerate(toRando):
+            
+            lock = threading.Lock()
+            
+            def process_file(args):
+                if cancel_event.is_set():
+                    return
+                i, orig_file = args
                 filename = orig_file.split(os.path.sep)[-1]
                 destfile = f'shuffled-{randomseed}'
                 for newpath in orig_file.split(os.path.sep)[1:]:
                     destfile = os.path.join(destfile, newpath)
 
+                if cancel_event.is_set():
+                    return
                 makepath(destfile)
                 shutil.copyfile(shufflelist[i], destfile)
 
@@ -167,58 +213,120 @@ with tqdm(total=total_steps, desc="Overall Progress") as overall_pbar:
                 if os.path.exists(mcmeta):
                     shutil.copyfile(mcmeta, mcdest)
 
-                pbar.set_postfix(file=filename)
-                pbar.update(1)
+                with lock:
+                    pbar.set_postfix(file=filename)
+                    pbar.update(1)
+            
+            # Process files using thread pool
+            with ThreadPoolExecutor(max_workers=4) as executor:
+                try:
+                    executor.map(process_file, enumerate(toRando))
+                except KeyboardInterrupt:
+                    cancel_event.set()
+                    raise
+            
+            if cancel_event.is_set():
+                raise KeyboardInterrupt
+        
         overall_pbar.update(1)
 
     if text_task:
-        with tqdm(total=len(languages) + len(specialtexts), desc="Randomising text") as pbar:
-            langvalues = []
+        langvalues = []
 
-            for lang in languages:
-                with open(lang, encoding='utf-8') as f:
-                    data = json.load(f)
-                langvalues += list(data.values())
+        for lang in langs_to_process:
+            with open(lang, encoding='utf-8') as f:
+                data = json.load(f)
+            langvalues += list(data.values())
 
-            for tfile in specialtexts:
-                with open(tfile, encoding='utf-8') as f:
-                    content = f.readlines()
-                content = [x.strip() for x in content]
-                langvalues += content
+        for tfile in specialtexts:
+            with open(tfile, encoding='utf-8') as f:
+                content = f.readlines()
+            content = [x.strip() for x in content]
+            langvalues += content
 
-            for lang in languages:
+        total_strings = 0
+        for lang in langs_to_process:
+            with open(lang, encoding='utf-8') as f:
+                total_strings += len(json.load(f))
+        for tfile in specialtexts:
+            with open(tfile, encoding='utf-8') as f:
+                total_strings += len(f.readlines())
+        
+        with tqdm(total=total_strings, desc="Randomising text") as pbar:
+            # Thread-safe processing of files
+            lock = threading.Lock()
+            
+            def process_language_file(lang):
+                if cancel_event.is_set():
+                    return
                 filename = lang.split(os.path.sep)[-1]
                 with open(lang, encoding='utf-8') as f:
                     data = json.load(f)
+                
+                # Create a thread-local random generator for this worker
+                thread_rng = random.Random(randomseed + hash(lang) % 1000000)
                 shufflelang = {}
+                
+                local_langvalues = list(langvalues)
                 for key in list(data.keys()):
-                    randindex = random.randint(0, len(langvalues)-1)
-                    shufflelang[key] = langvalues[randindex]
-                    del langvalues[randindex]
+                    if cancel_event.is_set():
+                        return
+                    randindex = thread_rng.randint(0, len(local_langvalues) - 1)
+                    shufflelang[key] = local_langvalues[randindex]
+                    del local_langvalues[randindex]
+                    
+                    with lock:
+                        pbar.set_postfix(file=filename)
+                        pbar.update(1)
+                
                 destpath = f'shuffled-{randomseed}' + lang[4:]
                 makepath(destpath)
                 with open(destpath, 'w', encoding='utf-8') as output:
                     json.dump(shufflelang, output)
-                pbar.set_postfix(file=filename)
-                pbar.update(1)
-
-            for tfile in specialtexts:
+            
+            def process_text_file(tfile):
+                if cancel_event.is_set():
+                    return
                 filename = tfile.split(os.path.sep)[-1]
                 with open(tfile, encoding='utf-8') as f:
                     content = f.readlines()
+                content = [x.strip() for x in content]
+                
+                # Create a thread-local random generator for this worker
+                thread_rng = random.Random(randomseed + hash(tfile) % 1000000)
                 linesadded = 0
                 outputlines = []
+                
+                local_langvalues = list(langvalues)
                 while linesadded < len(content):
-                    randindex = random.randint(0, len(langvalues)-1)
-                    outputlines.append(langvalues[randindex])
-                    del langvalues[randindex]
+                    if cancel_event.is_set():
+                        return
+                    randindex = thread_rng.randint(0, len(local_langvalues) - 1)
+                    outputlines.append(local_langvalues[randindex])
+                    del local_langvalues[randindex]
+                    
+                    with lock:
+                        pbar.set_postfix(file=filename)
+                        pbar.update(1)
+                    
                     linesadded += 1
+                
                 destpath = f'shuffled-{randomseed}' + tfile[4:]
                 makepath(destpath)
                 with open(destpath, 'w', encoding='utf-8') as output:
                     output.write('\n'.join(outputlines))
-                pbar.set_postfix(file=filename)
-                pbar.update(1)
+            
+            # Process files using thread pool
+            with ThreadPoolExecutor(max_workers=4) as executor:
+                try:
+                    lang_futures = [executor.submit(process_language_file, lang) for lang in langs_to_process]
+                    text_futures = [executor.submit(process_text_file, tfile) for tfile in specialtexts]
+                    for future in lang_futures + text_futures:
+                        future.result()
+                except KeyboardInterrupt:
+                    cancel_event.set()
+                    raise
+        
         overall_pbar.update(1)
 
 print("Creating meta files")
