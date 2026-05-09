4b14bc3d8dce7cd87795511d7238d46513a7f825
ElliNet13 (PC) <elliotmoose4@gmail.com>
2026-05-08 19:52:36 -0600

    Add progress bars with tqdm

--- resource_randomiser.py
+++ resource_randomiser.py
@@ -4,6 +4,7 @@ import random
 import sys
 import json
 import argparse
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Randomise the assets of a Minecraft resource pack.')
 parser.add_argument('-p', '--pack', default='pack', type=str, dest='pack', help='specifies a resource pack folder')
@@ -35,16 +36,6 @@ alttextures = args.alttextures
 auto_install = args.install
 
 random.seed(randomseed)
-
-def print2(toprint, toupdate=False):
-    global longestbar
-    if toupdate:
-        if len(toprint) > longestbar:
-            longestbar = len(toprint)
-        print(toprint.ljust(longestbar, ' '), end='\r', flush=True)
-    else:
-        print(toprint.ljust(longestbar, ' '))
-        longestbar = 0
 
 if resourcepack == "shuffle":
     print("The input resource pack may not be named 'shuffle'.")
@@ -135,117 +126,102 @@ for dirpath, dirs, files in os.walk(os.path.join(resourcepack,"assets")):
         elif dirpath == os.path.join(mcraft,'shaders','program') and randomiseshaders:
             shaders[file.split('.')[1]].append(fullfilepath)
 
-print2(f"Random Seed: {randomseed}")
-def shuffler(randoBool, toRando, inputType):
-    if randoBool and toRando != []:
-        print2("Randomising "+inputType, True)
-        shufflelist = list(toRando)
-        random.shuffle(shufflelist)
-        filecount = 0
-        while filecount < len(toRando):
-            destfile = 'shuffled'
-            for newpath in toRando[filecount].split(os.path.sep)[1:]:
-                destfile = os.path.join(destfile,newpath)
+print(f"Random Seed: {randomseed}")
 
-            progressbar = f"Randomising {inputType}: {filecount+1} of {len(toRando)}: {shufflelist[filecount].split(os.path.sep)[::-1][0]} -> {destfile.split(os.path.sep)[::-1][0]}"
-            print2(progressbar, True)
-
-            makepath(destfile)
-            shutil.copyfile(shufflelist[filecount], destfile)
-
-            #for textures
-            mcmeta = shufflelist[filecount]+'.mcmeta'
-            mcdest = destfile+'.mcmeta'
-            if os.path.exists(mcmeta):
-                shutil.copyfile(mcmeta, mcdest)
-
-            #model file
-            #origmodel = textures[texturenum].split('/')[::-1][0].split('.')[0]+'.json'
-            #shufmodel = shuffled[texturenum].split('/')[::-1][0].split('.')[0]+'.json'
-            #blockpath = '/assets/minecraft/models/block/'
-            #itempath = '/assets/minecraft/models/item/'
-            #makepath('shuffled'+blockpath)
-            #makepath('shuffled'+itempath)
-            #if os.path.exists(resourcepack+blockpath+shufmodel):
-            #    shutil.copyfile(resourcepack+blockpath+shufmodel, 'shuffled'+blockpath+origmodel)
-            #if os.path.exists(resourcepack+itempath+shufmodel):
-            #    shutil.copyfile(resourcepack+itempath+shufmodel, 'shuffled'+itempath+origmodel)
-
-            #blockstates
-            #statedir = '/assets/minecraft/blockstates/'
-            #makepath('shuffled'+statedir)
-            #if os.path.exists(resourcepack+statedir+shufmodel):
-            #    shutil.copyfile(resourcepack+statedir+shufmodel,'shuffled'+statedir+origmodel)
-
-            filecount += 1
-        print2('Randomised '+inputType)
-
+tasks = []
 randoimagery = randomisetextures or randomisefont
 for resolution, imagefiles in images.items():
-    shuffler(randoimagery, imagefiles, f"textures ({resolution})")
-shuffler(randomisemodels, itemmodels, "item models")  # can reduce more repeat code with a dict (ie unused tags code in data randomiser)
-shuffler(randomisemodels, blockmodels, "block models")
-shuffler(randomiseblockstates, blockstates, "block states")
-shuffler(randomisesounds, sounds, "sounds")
+    if randoimagery and imagefiles:
+        tasks.append((imagefiles, f"textures ({resolution})"))
+if randomisemodels and itemmodels:
+    tasks.append((itemmodels, "item models"))
+if randomisemodels and blockmodels:
+    tasks.append((blockmodels, "block models"))
+if randomiseblockstates and blockstates:
+    tasks.append((blockstates, "block states"))
+if randomisesounds and sounds:
+    tasks.append((sounds, "sounds"))
 for key, value in shaders.items():
-    shuffler(randomiseshaders, value, f"shaders ({key})")
+    if randomiseshaders and value:
+        tasks.append((value, f"shaders ({key})"))
 
-if randomisetext and (languages != [] or specialtexts != []):
-    print2("Randomising text", True)
-    langvalues = []
+text_task = randomisetext and (languages or specialtexts)
+total_steps = len(tasks) + (1 if text_task else 0)
 
-    for lang in languages:
-        with open(lang, encoding='utf-8') as f:
-            data = json.load(f)
-        langvalues += list(data.values())
+with tqdm(total=total_steps, desc="Overall Progress") as overall_pbar:
+    for toRando, inputType in tasks:
+        with tqdm(total=len(toRando), desc=inputType) as pbar:
+            shufflelist = list(toRando)
+            random.shuffle(shufflelist)
+            for i, orig_file in enumerate(toRando):
+                filename = orig_file.split(os.path.sep)[-1]
+                destfile = 'shuffled'
+                for newpath in orig_file.split(os.path.sep)[1:]:
+                    destfile = os.path.join(destfile, newpath)
 
-    for tfile in specialtexts:
-        with open(tfile, encoding='utf-8') as f:
-            content = f.readlines()
-        content = [x.strip() for x in content]
-        langvalues += content
+                makepath(destfile)
+                shutil.copyfile(shufflelist[i], destfile)
 
-    processedlangs = 0
-    totallangs = len(languages) + len(specialtexts)
-    for lang in languages:
-        processedlangs += 1
-        progressbar = f"Randomising text: {processedlangs} of {totallangs}: {lang.split(os.path.sep)[::-1][0]}"
-        print2(progressbar, True)
+                mcmeta = shufflelist[i] + '.mcmeta'
+                mcdest = destfile + '.mcmeta'
+                if os.path.exists(mcmeta):
+                    shutil.copyfile(mcmeta, mcdest)
 
-        with open(lang, encoding='utf-8') as f:
-            data = json.load(f)
-        shufflelang = {}
-        for key in list(data.keys()):
-            randindex = random.randint(0, len(langvalues)-1)
-            shufflelang[key] = langvalues[randindex]
-            del langvalues[randindex]
-        destpath = 'shuffled'+lang[4:]
-        makepath(destpath)
-        with open(destpath, 'w', encoding='utf-8') as output:
-            json.dump(shufflelang, output)
+                pbar.set_postfix(file=filename)
+                pbar.update(1)
+        overall_pbar.update(1)
 
-    for tfile in specialtexts:
-        processedlangs += 1
-        progressbar = f"Randomising text: {processedlangs} of {totallangs}: {lang.split(os.path.sep)[::-1][0]}"
-        print2(progressbar, True)
+    if text_task:
+        with tqdm(total=len(languages) + len(specialtexts), desc="Randomising text") as pbar:
+            langvalues = []
 
-        with open(tfile, encoding='utf-8') as f:
-            content = f.readlines()
-        linesadded = 0
-        outputlines = []
-        while linesadded < len(content):
-            randindex = random.randint(0, len(langvalues)-1)
-            outputlines.append(langvalues[randindex])
-            del langvalues[randindex]
-            linesadded += 1
-        destpath = 'shuffled'+tfile[4:]
-        makepath(destpath)
-        with open(destpath, 'w', encoding='utf-8') as output:
-            output.write('\n'.join(outputlines))
+            for lang in languages:
+                with open(lang, encoding='utf-8') as f:
+                    data = json.load(f)
+                langvalues += list(data.values())
 
-    print2("Randomised text")
+            for tfile in specialtexts:
+                with open(tfile, encoding='utf-8') as f:
+                    content = f.readlines()
+                content = [x.strip() for x in content]
+                langvalues += content
 
-print2("Creating meta files")
+            for lang in languages:
+                filename = lang.split(os.path.sep)[-1]
+                with open(lang, encoding='utf-8') as f:
+                    data = json.load(f)
+                shufflelang = {}
+                for key in list(data.keys()):
+                    randindex = random.randint(0, len(langvalues)-1)
+                    shufflelang[key] = langvalues[randindex]
+                    del langvalues[randindex]
+                destpath = 'shuffled' + lang[4:]
+                makepath(destpath)
+                with open(destpath, 'w', encoding='utf-8') as output:
+                    json.dump(shufflelang, output)
+                pbar.set_postfix(file=filename)
+                pbar.update(1)
+
+            for tfile in specialtexts:
+                filename = tfile.split(os.path.sep)[-1]
+                with open(tfile, encoding='utf-8') as f:
+                    content = f.readlines()
+                linesadded = 0
+                outputlines = []
+                while linesadded < len(content):
+                    randindex = random.randint(0, len(langvalues)-1)
+                    outputlines.append(langvalues[randindex])
+                    del langvalues[randindex]
+                    linesadded += 1
+                destpath = 'shuffled' + tfile[4:]
+                makepath(destpath)
+                with open(destpath, 'w', encoding='utf-8') as output:
+                    output.write('\n'.join(outputlines))
+                pbar.set_postfix(file=filename)
+                pbar.update(1)
+        overall_pbar.update(1)
+
+print("Creating meta files")
 if "16x16" in images.keys():
     shutil.copyfile(random.choice(images["16x16"]), os.path.join('shuffled','pack.png'))
 elif os.path.exists(os.path.join(resourcepack,'pack.png')):
@@ -256,7 +232,7 @@ with open(os.path.join('shuffled','pack.mcmeta'), "w") as descfile:
     descfile.write('{"pack":{"pack_format":4,"description":"Minecraft Shuffled by lexikiq"}}')
 
 if auto_install:
-    print2('Installing to resource pack folder')
+    print('Installing to resource pack folder')
 
     try:
         system = sys.platform.lower()
@@ -268,17 +244,17 @@ if auto_install:
             destfolder = os.path.expandvars(os.path.join('%APPDATA%', '.minecraft', 'resourcepacks', 'shuffle'))
         else:
             destfolder = 'shuffle'
-            print2('Failed to identify operating system, placing file in current folder instead.')
+            print('Failed to identify operating system, placing file in current folder instead.')
         shutil.make_archive(destfolder, 'zip', 'shuffled')
-        print2('Resource pack installed!')
+        print('Resource pack installed!')
         shutil.rmtree('shuffled')
     except:
-        print2('Compression failed! Please manually move the "shuffled" folder to your resource pack folder.')
+        print('Compression failed! Please manually move the "shuffled" folder to your resource pack folder.')
 else:
-    print2('Creating shuffled pack archive')
+    print('Creating shuffled pack archive')
     try:
         shutil.make_archive('shuffle', 'zip', 'shuffled')
-        print2('Shuffled pack created as shuffle.zip')
+        print('Shuffled pack created as shuffle.zip')
         shutil.rmtree('shuffled')
     except:
-        print2('Compression failed! The "shuffled" folder contains the randomized pack.')
+        print('Compression failed! The "shuffled" folder contains the randomized pack.')
